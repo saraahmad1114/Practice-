cc7dbaef38b3c14272bc975afc7732343d2f2b5d
Sara <saraahmad1114@yahoo.com>
2017-09-19 23:11:35 -0400

    second

--- 2.playground/Contents.swift
+++ 2.playground/Contents.swift
@@ -3,3 +3,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//2. “Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.”
+
+func isStringPalindrome(givenString: String) -> Bool {
+    return String(givenString.characters).lowercased() == String(givenString.characters.reversed()).lowercased()
+}
+
+isStringPalindrome(givenString: "racecar")
+
+//returns true
+
+isStringPalindrome(givenString: "Racecar")
+
+//returns true 

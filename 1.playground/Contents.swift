ffff19b2e55e361d709890542becf8a33c694d2c
Sara <saraahmad1114@yahoo.com>
2017-09-19 23:06:35 -0400

    second way

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -6,7 +6,28 @@ var str = "Hello, playground"
 
 //1. “Write a function that accepts a String as its only parameter, and returns true if the string has only unique letters, taking letter case into account”
 
+//First Method:
 
 func determineUniqueness(givenString: String) -> Bool{
     return givenString.characters.count == Set(givenString.characters).count
 }
+
+//Second Method: 
+
+func determineUniquenessTwo(givenString: String) -> Bool {
+    
+    var uniqueString = [String]()
+    
+    for char in givenString.characters{
+        if uniqueString.contains(String(char)){
+            print("present")
+        }
+        else {
+            uniqueString.append(String(char))
+        }
+    }
+    return givenString.characters.count == uniqueString.count
+}
+
+
+

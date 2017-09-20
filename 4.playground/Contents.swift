4f5a41170931951fb4ec38bbdc386d8d517bd8d0
Sara <saraahmad1114@yahoo.com>
2017-09-20 12:46:41 -0400

    4

--- 4.playground/Contents.swift
+++ 4.playground/Contents.swift
@@ -3,3 +3,16 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//4. “Write your own version of the contains() method on String that ignores letter case, and without using the existing contains() method”
+
+extension String {
+    func stringContains(_ string: String) -> Bool {
+        return range(of: string, options: .caseInsensitive) != nil
+    }
+}
+
+"Hello, World".stringContains("Hello")
+
+//returns true 
+

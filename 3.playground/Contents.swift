f5926e2d40c396a57682fde4a9f0b1a1c5ed8a0d
Sara <saraahmad1114@yahoo.com>
2017-09-19 23:13:25 -0400

    added 3

--- 3.playground/Contents.swift
+++ 3.playground/Contents.swift
@@ -3,3 +3,9 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//3. “Write a function that accepts two String parameters, and returns true if they contain the same characters in any order taking into account letter case”
+
+
+
+

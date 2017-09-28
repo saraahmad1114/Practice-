b4db1978e811be93ba1b7f49d4976e6da9e82e6e
Sara <saraahmad1114@yahoo.com>
2017-09-27 22:22:07 -0400

    done with 6

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -3,3 +3,41 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question 6: Write a function that accepts a string as its input, and returns the same string just with duplicate letters removed
+
+//first way:
+
+func removeDuplicates (string: String) -> String {
+    
+    return String(Set(string.characters))
+}
+
+removeDuplicates(string: "Sara")
+
+//"rSa"
+
+//removes duplicates 
+
+//second way: 
+
+func removeDuplicatesTwo (string: String) -> [String]{
+    
+    var holder = [String]()
+    
+    for char in string.characters{
+        var stringChar = String(char)
+        
+        if holder.contains(stringChar){
+            print("already present")
+        }
+        else {
+            holder.append(stringChar)
+        }
+    }
+    return holder
+}
+
+
+
+

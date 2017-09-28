588c4607babd6d14a23c61a97844d56b7021e732
Sara <saraahmad1114@yahoo.com>
2017-09-27 22:29:27 -0400

    done with 7

--- 7.playground/Contents.swift
+++ 7.playground/Contents.swift
@@ -6,4 +6,20 @@ var str = "Hello, playground"
 
 //Question 7: Write a function that returns a string with any consecutive spaces replaced with a single space
 
-
+func replaceConsecutiveSpacesIn (input: String) -> String {
+    var seenSpace = false
+    var returnValue = ""
+    
+    for letter in input.characters {
+        if letter == " " {
+            if seenSpace { continue }
+            seenSpace = true
+        } else {
+            seenSpace = false
+        }
+        
+        returnValue.append(letter)
+    }
+    
+    return returnValue
+}

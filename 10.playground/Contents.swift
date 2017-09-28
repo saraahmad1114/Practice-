e7083c1c069915f99756a4a6b764ad49e072b908
Sara <saraahmad1114@yahoo.com>
2017-09-28 16:59:28 -0400

    done with 10

--- 10.playground/Contents.swift
+++ 10.playground/Contents.swift
@@ -3,3 +3,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Given a string in English, return a tuple containing the number of vowels and consonants
+
+func giveNumberOfVowelsAndConsonants (string: String) -> (Int, Int){
+    
+    var vowelsAndConsonants = (0, 0)
+    
+    var vowels = ["a", "e", "i", "o", "u"]
+    
+    for char in string.characters{
+        var newChar = String(char)
+        if vowels.contains(newChar){
+            vowelsAndConsonants.0 += 1
+        }
+        else {
+            vowelsAndConsonants.1 += 1
+        }
+    }
+    return vowelsAndConsonants
+}
+
+giveNumberOfVowelsAndConsonants(string: "Swift Coding Challenges")
+
+//returns 6 vowels and 17 consonants 
+
+giveNumberOfVowelsAndConsonants(string: "Mississippi")
+
+//returns 4 vowels and 7 consonants 
+
+

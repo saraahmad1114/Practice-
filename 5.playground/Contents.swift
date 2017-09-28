2f91e3354f7aac1f07b59f2e2fcb68988ca0f18a
Sara <saraahmad1114@yahoo.com>
2017-09-27 20:05:16 -0400

    done with 5

--- 5.playground/Contents.swift
+++ 5.playground/Contents.swift
@@ -5,3 +5,47 @@ import UIKit
 var str = "Hello, playground"
 
 //Question: Write a function that accepts a string, and returns how many times a specific character appears, taking case into account
+
+func createStringDictionary (string: String) -> [Character: Int]{
+    
+    var dictionary = [Character: Int]()
+    
+    for char in string.characters{
+        if dictionary[char] != nil {
+            dictionary[char] = dictionary[char]! + 1
+        }
+        else {
+            dictionary[char] = 1
+        }
+    }
+    return dictionary
+
+}
+
+
+func charAppearsNumberOfTimesIn (string: String, char: Character) -> Int? {
+    
+    var dictionary = createStringDictionary(string: string)
+    
+    for (key, value) in dictionary {
+    
+        if key == char {
+            return dictionary[key]
+        }
+    }
+    return nil
+}
+
+charAppearsNumberOfTimesIn(string: "The rain in Spain", char: "a")
+
+//returns 2 
+
+charAppearsNumberOfTimesIn(string: "Mississippi", char: "i")
+
+//returns 4 
+
+charAppearsNumberOfTimesIn(string: "Hacking with Swift", char: "i")
+
+//returns 3 
+
+

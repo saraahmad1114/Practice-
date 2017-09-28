07181c94c2608de24dbd3d74245d05a1515d9f32
Sara <saraahmad1114@yahoo.com>
2017-09-28 13:30:23 -0400

    trying to figure out

--- 8.playground/Contents.swift
+++ 8.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question 8: Write a function that accepts two strings, and returns true if one string is rotation of the other, taking letter case into account
+
+func areStringsRotation(str1: String, str2: String) -> Bool
+{
+    var str1str1 = str1.characters + str1.characters
+    
+    var str2str2 = str2.characters + str2.characters
+    
+    var charStr1 = str1.characters
+    
+    var charStr2 = str2.characters
+    
+    return str1str1.contains(charStr1) || str2str2.contains(charStr2)
+}

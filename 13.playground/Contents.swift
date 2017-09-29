01c6883e49456f9fc832017a7d0b2b73a9eb4ca5
Sara <saraahmad1114@yahoo.com>
2017-09-29 09:42:18 -0400

    done with 14

--- 13.playground/Contents.swift
+++ 13.playground/Contents.swift
@@ -4,4 +4,29 @@ import UIKit
 
 var str = "Hello, playground"
 
+func stringCompression(input: String) -> String {
+    var currentLetter: Character?
+    var returnValue = ""
+    var letterCounter = 0
+    
+    for letter in input.characters {
+        if letter == currentLetter {
+            letterCounter += 1
+        } else {
+            if let current = currentLetter {
+                returnValue.append("\(current)\(letterCounter)")
+            }
+            
+            currentLetter = letter
+            letterCounter = 1
+        }
+    }
+    
+    if let current = currentLetter {
+        returnValue.append("\(current)\(letterCounter)")
+    }
+    
+    return returnValue
+}
+
 

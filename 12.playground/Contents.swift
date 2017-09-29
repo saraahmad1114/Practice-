77d4dd7b293f351b12fddaf69aad7fa7da0113db
Sara <saraahmad1114@yahoo.com>
2017-09-29 09:31:07 -0400

    done with 12

--- 12.playground/Contents.swift
+++ 12.playground/Contents.swift
@@ -3,3 +3,31 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question 12: Write a function that accepts a string of words with a similar prefix, separated by spaces, and returns the longest substring that prefixes all words. 
+
+func findLongestPrefixIn (string: String) -> String{
+
+    let parts = string.components(separatedBy: " ")
+    guard let first = parts.first else { return "" }
+    
+    var currentPrefix = ""
+    var bestPrefix = ""
+    
+    for letter in first.characters {
+        currentPrefix.append(letter)
+        
+        for word in parts {
+            if !word.hasPrefix(currentPrefix) {
+                return bestPrefix
+            }
+        }
+        
+        bestPrefix = currentPrefix
+    }
+    
+    return bestPrefix
+}
+
+
+
